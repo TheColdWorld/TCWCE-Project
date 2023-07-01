f7187fd656bbaf3dd5f7dda681f8a9927262b1ef
TheColdWorld <3264524796@qq.com>
2023-07-01 11:46:51 +0800

    Upadte Rust Dev [Version 2.0.0 Last art]

--- src/rust/src/Headers.rs
+++ src/rust/src/Headers.rs
@@ -17,7 +17,6 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
     USA
 */
-
 pub struct Headers {
     #[allow(non_snake_case)]
     _Headers: Vec<crate::Header::Header>,
@@ -183,5 +182,96 @@ impl Headers {
             _Headers: _Headerss.clone(),
         });
     }
+    pub fn  Length(&self) -> usize{self._Headers.len()}
 }
+#[allow(non_snake_case)]
+impl Clone for Headers{
+    fn clone(&self) -> Self {
+        Self{
+            _Headers:self._Headers.clone()
+        }
+    }
+}
+#[allow(non_snake_case)]
+impl crate::API::Index<&str,crate::Header::Header> for Headers{
+    fn index(&mut self, INDEX: &str) -> crate::Header::Header {
+        for index in &self._Headers {
+            if index.Name() .eq( INDEX) {return index.clone()}
+        };
+        panic!("Item Not Found!");
+    }
+    fn GetIndex(&mut self, INDEX: &str) -> Result<crate::Header::Header, String> {
+        for index in &self._Headers {
+            if index.Name() .eq(INDEX) {return Ok(index.clone())}
+        };
+        return  Err("Item Not Found!".to_string());
+    }
+    unsafe fn GetIndexPointer(&mut self, INDEX: &str) -> *mut crate::Header::Header {
+        for index in &mut self._Headers {
+            if index.Name() .eq( INDEX) {return index as *mut crate::Header::Header}
+        };
+        panic!("Item Not Found!");
+    }
+    fn GetIndexRef(&mut self, INDEX: &str) -> &crate::Header::Header {
+        for index in &self._Headers {
+            if index.Name() .eq( INDEX) {return index}
+        };
+        panic!("Item Not Found!");
+    }
+}
+#[allow(non_snake_case)]
+impl crate::API::Index<String,crate::Header::Header> for  Headers{
+    fn index(&mut self, INDEX: String) -> crate::Header::Header {
+        for index in &self._Headers {
+            if index.Name() .eq( &INDEX) {return index.clone()}
+        };
+        panic!("Item Not Found!");
+    }
+    fn GetIndex(&mut self, INDEX: String) -> Result<crate::Header::Header, String> {
+        for index in &self._Headers {
+            if index.Name() .eq(&INDEX) {return Ok(index.clone())}
+        };
+       return  Err("Item Not Found!".to_string());
+    }
+    unsafe fn GetIndexPointer(&mut self, INDEX: String) -> *mut crate::Header::Header {
+        for index in &mut self._Headers {
+            if index.Name() .eq( &INDEX) {return  index as *mut crate::Header::Header}
+        };
+        panic!("Item Not Found!");
+    }
+    fn GetIndexRef(&mut self, INDEX: String) -> &crate::Header::Header {
+        for index in &self._Headers {
+            if index.Name() .eq( &INDEX) {return index}
+        };
+        panic!("Item Not Found!");
+    }
+}
+impl crate::API::Index<usize,crate::Header::Header> for  Headers{
+    fn index(&mut self, INDEX: usize) -> crate::Header::Header {
+        if INDEX >= self.Length() {
+            panic!("Out of Index");
+        };
+        return  self._Headers[INDEX].clone();
+    }
 
+    fn GetIndex(&mut self, INDEX: usize) -> Result<crate::Header::Header, String> {
+        if INDEX >= self.Length() {
+            return Err("Out of Index".to_string())
+        };
+        return Ok({self._Headers[INDEX].clone()});
+    }
+
+    unsafe fn GetIndexPointer(&mut self, INDEX: usize) -> *mut crate::Header::Header {
+        if INDEX >= self.Length() {
+            panic!("Out of Index");
+        };
+        return &mut self._Headers[INDEX] as *mut crate::Header::Header;
+    }
+
+    fn GetIndexRef(&mut self, INDEX: usize) -> &crate::Header::Header {
+        if INDEX >= self.Length() {
+            panic!("Out of Index");
+        };
+        return &self._Headers[INDEX];
+    }
+}
